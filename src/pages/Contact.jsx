76802831ede782638aa592bd2d624bf6ad63672a
cagatayyildiz <yildiz.cagatay@hotmail.com>
2024-07-23 09:44:32 +0300

    input-state sample

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Contact() {
-  return (
-    <div>Contact</div>
-  )
+
+  const [users, setusers] = useState([])
+  const [name, setname] = useState("")
+
+  const add = () => {
+    setusers([...users, name])
+    setname("")
+  }
+
+  return <>
+    <div>
+      <label htmlFor="">Name</label>
+      <input type='text' value={name} onChange={(e) => setname(e.target.value)} />
+    </div>
+    <div>
+      <button onClick={add}>Add</button>
+    </div>
+    <ul>
+      {
+        users && users.map(item => <li>{item}</li>)
+      }
+    </ul>
+
+  </>
 }
 
 export default Contact
